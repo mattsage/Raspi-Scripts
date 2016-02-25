a8cf9a09414bbcb065f8a7df6fb8908cfdebf54d
Matt Sage <mattsage@rocketmail.com>
2016-02-25 21:30:45 +0000

    Adding Script

--- PIR.py
+++ PIR.py
@@ -8,6 +8,7 @@ count = 0
 
 while (count < 15):
 	if pir.motion_detected:
-		os.system("echo `date` >> motion.txt")
+		os.system("echo `date` >> motiondetected.txt")
 		time.sleep(600)
 		count = count + 1
+	
