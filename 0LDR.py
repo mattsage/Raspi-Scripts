c5ca0fdf8f071805af6b3d63bf51031f7921c617
Matthew Sage <mattsage@users.noreply.github.com>
2016-02-26 10:51:51 +0000

    Create 0LDR.py

---
+++ 0LDR.py
@@ -0,0 +1,13 @@
+************************
+Wait for light and dark:
+************************
+
+from gpiozero import LightSensor
+
+sensor = LightSensor(18)
+
+while True:
+    sensor.wait_for_light()
+    print("It's light! :)")
+    sensor.wait_for_dark()
+    print("It's dark :(")
