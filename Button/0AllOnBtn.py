45f1579884b21251eb5b057c0fa8726a7b53ab6e
Matthew Sage <mattsage@users.noreply.github.com>
2016-02-26 10:54:12 +0000

    Create 0AllOnBtn.py

---
+++ Button/0AllOnBtn.py
@@ -0,0 +1,20 @@
+from gpiozero import LED, Buzzer, Button
+
+button = Button(2)
+buzzer = Buzzer(3)
+red = LED(4)
+amber = LED(5)
+green = LED(6)
+
+things = [red, amber, green, buzzer]
+
+def things_on():
+    for thing in things:
+        thing.on()
+
+def things_off():
+    for thing in things:
+        thing.off()
+
+button.when_pressed = things_on
+button.when_released = things_off
