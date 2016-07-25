ad4d16ebe5793e9af305ea255ffb99eda745c880
mattsage <mattsage@rocketmail.com>
2016-07-25 19:05:31 +0000

    working

--- ProtoZero/BtnBoard.py
+++ ProtoZero/BtnBoard.py
@@ -7,7 +7,9 @@ button3 = Button(25)
 button4 = Button(5)
 button5 = Button(19)
 
-clear
+def start() :
+
+	start()
 print ("Please Choose a Button")
 print ("1) Kodi")
 print ("2) Desktop")
@@ -17,26 +19,27 @@ print ("5) RetroPie")
 
 while True:
         if button.is_pressed:
-                os.system("echo kodi")
-                #os.system("kodi")
+        	#os.system("echo kodi")
+                os.system("kodi")
                 exit()
 
         if button2.is_pressed:
-                os.system("echo startx")
-                #os.system("startx")
+        	#os.system("echo startx")
+        	os.system("startx")
                 exit()
                 
         if button3.is_pressed:
-                os.system("echo poweroff")
-                #os.system("sudo poweroff")
+        	#os.system("echo poweroff")
+                os.system("sudo poweroff")
                 exit()
                 
         if button4.is_pressed:
-                os.system("echo Controller Connect")
-                #os.system("./home/pi/ConntrollerConnect.sh")
-                exit()
-                
+                #os.system("echo Controller Connect")
+                os.system("/home/pi/RetropiAutoConnect.sh")
+		print ("Did it connect? If so Press 5 (if not press 4 again)")
+                continue
+
         if button5.is_pressed:
-                os.system("echo emulationstation")
-                #os.system("emulationstation")
+                #os.system("echo emulationstation")
+                os.system("emulationstation")
                 exit()
