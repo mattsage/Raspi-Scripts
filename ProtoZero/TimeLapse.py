bf8805c67d06770c0ea396dd6d7141549a4da82f
Matthew Sage <mattsage@users.noreply.github.com>
2016-07-27 17:00:29 +0100

    Create TimeLapse.py
    First Temp File

---
+++ ProtoZero/TimeLapse.py
@@ -0,0 +1,43 @@
+from gpiozero import Button
+import os
+
+button = Button(4)
+button2 = Button(17)
+button3 = Button(25)
+button4 = Button(5)
+button5 = Button(19)
+
+
+print ("Please Choose a Button")
+print ("1) ")
+print ("2) ")
+print ("3) Power Off")
+print ("4) ")
+print ("5) ")
+
+while True:
+        if button.is_pressed:
+        	#os.system("echo kodi")
+                os.system("kodi")
+                exit()
+
+        if button2.is_pressed:
+        	#os.system("echo startx")
+        	os.system("startx")
+                exit()
+                
+        if button3.is_pressed:
+        	#os.system("echo poweroff")
+                os.system("sudo poweroff")
+                exit()
+                
+        if button4.is_pressed:
+                #os.system("echo Controller Connect")
+                os.system("/home/pi/RetropiAutoConnect.sh")
+		print ("Did it connect? If so Press 5 (if not press 4 again)")
+                continue
+
+        if button5.is_pressed:
+                #os.system("echo emulationstation")
+                os.system("emulationstation")
+                exit()
