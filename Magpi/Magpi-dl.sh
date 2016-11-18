6f1f5c132a06d6d527226bfd775c0c40cd49ae26
Matthew Sage <mattsage@users.noreply.github.com>
2016-11-18 16:04:38 +0000

    Create Magpi-dl.sh

---
+++ Magpi/Magpi-dl.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#Download all PDF's
+wget -A pdf -m -p -E -k -K -np https://www.raspberrypi.org/magpi-issues/
+
+#Download Specific = wget [URL] e.g:
+#wget https://raspberrypi.org/magpi-issues/Projects_Book_v1.pdf
