cf1b9fc9d5b53adcab69e9a664a09c3657311ef7
mattsage <mattsage@rocketmail.com>
2016-11-21 21:24:46 +0000

    blink1 youtube

---
+++ youtube-dl/blink1YT-Download.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "Video or Audio (A/V?)"
+read choice
+
+if [ $choice = "A" ] #Option A: Audio
+	then
+	echo "Please enter URL"
+	read URL
+	youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 "$URL"
+	sudo /home/pi/blink1/commandline/blink1-tool --green --glimmer=50
+elif [ $choice = "V" ] #Option V: Video
+        then
+        echo "Please enter URL"
+        read URL
+        youtube-dl -f 22 "$URL"
+	sudo /home/pi/blink1/commandline/blink1-tool --green --glimmer=50
+else
+	echo "Invalid Choice"
+	exit 
+fi
