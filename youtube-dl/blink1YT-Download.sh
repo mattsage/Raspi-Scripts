9abb61c1a8c97e6a942b8a393126418753ab5267
mattsage <mattsage@rocketmail.com>
2016-12-05 22:24:56 +0000

    typo

--- youtube-dl/blink1YT-Download.sh
+++ youtube-dl/blink1YT-Download.sh
@@ -14,7 +14,7 @@ if [ $choice = "A" ] || [ $choice = "a" ] #Option A: Audio
 	youtube-dl --extract-audio --audio-format mp3 --audio-quality 0 "$URL"
 	#./home/pi/pyPushBullet/pushbullet_cmd.py $apikey note udeCmddJpl "Download Completed" "Download Completed"
 	sudo /home/pi/blink1/commandline/blink1-tool --green --glimmer=500000000x
-elif [ $choice = "V"] || [ $choice = "v" ] #Option V: Video
+elif [ $choice = "V" ] || [ $choice = "v" ] #Option V: Video
         then
         echo "Please enter URL"
         read URL
