75a48a1d4a81f9d6ad9cd3f1e8071511125ed9a5
Matthew Sage <mattsage@users.noreply.github.com>
2016-12-08 15:58:00 +0000

    Create Pyth9onTimer.py

---
+++ Python/Pyth9onTimer.py
@@ -0,0 +1,10 @@
+###########################
+use -  time python test.sh
+###########################
+
+from datetime import datetime
+startTime = datetime.now()
+
+#do something
+
+print datetime.now() - startTime
