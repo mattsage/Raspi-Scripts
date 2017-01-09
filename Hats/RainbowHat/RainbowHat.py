2283b131040ea55f4481a22805df25ec708d6c65
Matthew Sage <mattsage@users.noreply.github.com>
2017-01-09 21:06:20 +0000

    Update RainbowHat.py

--- Hats/RainbowHat/RainbowHat.py
+++ Hats/RainbowHat/RainbowHat.py
@@ -2,7 +2,9 @@
 
 import signal
 import rainbowhat
-import os
+import datetime
+
+timenow = datetime.datetime.now().strftime("%H%M")
 
 def display_message(message):
     rainbowhat.display.print_str(message)
@@ -10,22 +12,33 @@ def display_message(message):
 
 @rainbowhat.touch.A.press()
 def press_a(channel):
-    display_message("AHOY")
-	print ("Button A touched!")
+    rainbowhat.rainbow.clear()
+    display_message("Noah")
+    print ("Button A touched!")
+    rainbowhat.rainbow.set_all(255,0,0)
+    rainbowhat.rainbow.show()
     rainbowhat.lights.rgb(1,0,0)
 
 @rainbowhat.touch.B.press()
 def press_b(channel):
-    display_message("YARR")
-	print ("Button A touched!")
+    rainbowhat.rainbow.clear()
+    display_message("Jake")
+    print ("Button B touched!")
+    rainbowhat.rainbow.set_all(0,255,0)
+    rainbowhat.rainbow.show()
     rainbowhat.lights.rgb(0,1,0)
 
 @rainbowhat.touch.C.press()
 def press_c(channel):
-    display_message("GROG")
-	print ("Button A touched!")
+    rainbowhat.rainbow.clear()
+    display_message("Matt")
+    print ("Button C touched!")
+    rainbowhat.rainbow.set_all(0,0,255)
+    rainbowhat.rainbow.show()
     rainbowhat.lights.rgb(0,0,1)
 
 	
-display_message("ABC.")
+display_message(timenow)
 rainbowhat.lights.rgb(1,1,1)
+
+signal.pause()
