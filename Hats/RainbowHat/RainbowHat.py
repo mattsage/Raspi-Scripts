52ba4ea1035ebcd53d84baa5c6189203dab2a901
Matthew Sage <mattsage@users.noreply.github.com>
2017-01-11 11:58:19 +0000

    Update RainbowHat.py

--- Hats/RainbowHat/RainbowHat.py
+++ Hats/RainbowHat/RainbowHat.py
@@ -12,33 +12,42 @@ def display_message(message):
 
 @rainbowhat.touch.A.press()
 def press_a(channel):
-    rainbowhat.rainbow.clear()
-    display_message("NOAH")
-    print ("Button A touched!")
-    rainbowhat.rainbow.set_all(255,0,0)
+#    rainbowhat.rainbow.clear()
+    display_message("NOAH") #Display on LED Display
+    print ("Button A touched!") #Print to Terminal
+    rainbowhat.rainbow.set_all(255,0,0,brightness=0.1) #RGB LEDS - RED
     rainbowhat.rainbow.show()
-    rainbowhat.lights.rgb(1,0,0)
+    rainbowhat.lights.rgb(1,0,0)#Button Light
 
 @rainbowhat.touch.B.press()
 def press_b(channel):
-    rainbowhat.rainbow.clear()
-    display_message("Jake")
-    print ("Button B touched!")
-    rainbowhat.rainbow.set_all(0,255,0)
+#    rainbowhat.rainbow.clear()
+    display_message("Jake") 
+    print ("Button B touched!") 
+    rainbowhat.rainbow.set_all(0,255,0,brightness=0.1)
     rainbowhat.rainbow.show()
     rainbowhat.lights.rgb(0,1,0)
 
 @rainbowhat.touch.C.press()
 def press_c(channel):
-    rainbowhat.rainbow.clear()
-    display_message("Matt")
+#    rainbowhat.rainbow.clear()
+    display_message("Matt") 
     print ("Button C touched!")
-    rainbowhat.rainbow.set_all(0,0,255)
+    rainbowhat.rainbow.set_all(0,0,255,brightness=0.1)
     rainbowhat.rainbow.show()
     rainbowhat.lights.rgb(0,0,1)
-
+	
+@rainbowhat.touch.release()
+def release(channel):
+    print("Button release!")
+    rainbowhat.rainbow.clear()
+    rainbowhat.lights.rgb(0,0,0)
+    rainbowhat.buzzer.note(880,0.1)
 	
 display_message(timenow)
 rainbowhat.lights.rgb(1,1,1)
+rainbowhat.rainbow.set_all(0,0,255)
+rainbowhat.rainbow.set_pixel(3, r, g, b, brightness=0.1)
+
 
 signal.pause()
