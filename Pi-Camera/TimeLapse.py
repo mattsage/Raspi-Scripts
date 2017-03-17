372997ff184f1882f0dad8bcd6509e50ade92c26
mattsage <mattsage@rocketmail.com>
2017-03-17 15:32:36 +0000

    updates

--- Pi-Camera/TimeLapse.py
+++ Pi-Camera/TimeLapse.py
@@ -6,12 +6,12 @@ camera = PiCamera()
 camera.resolution = (1024, 768)
 loop = 0
 
-for i in range(100):
+for i in range(18):
 	camera.start_preview()
 	camera.capture('image{0:04d}.jpg'.format(i))
 	loop += 1
 	print loop, "Photo Taken"
-	sleep(1)
+	sleep(10)
 
 print "Converting JPG to GIF"
 system('convert -delay 10 -loop 0 image*.jpg animation.gif')
