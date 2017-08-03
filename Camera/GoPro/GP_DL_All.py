77401073720e672eebd59a85db27e6a1734d5199
Matthew Sage <mattsage@users.noreply.github.com>
2017-08-03 15:50:47 +0100

    Update GP_DL_All.py

--- Camera/GoPro/GP_DL_All.py
+++ Camera/GoPro/GP_DL_All.py
@@ -2,5 +2,6 @@ from goprocam import GoProCamera, constants
 gpCam = GoProCamera.GoPro()
 
 ## Downloads all of the SD card's contents and then formats the sd card.
-
+print "Downloading All GP Footage"
 gpCam.downloadAll()
+print "Download Completed"
